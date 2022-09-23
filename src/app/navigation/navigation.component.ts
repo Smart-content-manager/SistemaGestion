7f67c91c7e4e850904233cc57aaa75f1d435f15f
Death <ricardo.pajaro.bc2d@gmail.com>
2022-09-23 12:53:39 -0500

    adding override back click

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Observable} from 'rxjs';
 import {StorageService} from "../services/storage.service";
+import {LocationStrategy} from "@angular/common";
 
 @Component({
   selector: 'app-navigation',
@@ -10,8 +11,20 @@ import {StorageService} from "../services/storage.service";
 export class NavigationComponent {
   currentPath: Observable<string>
 
-  constructor(private storage: StorageService) {
+  constructor(
+    private storage: StorageService,
+    private location: LocationStrategy
+  ) {
     this.currentPath = this.storage.currentPath
+    // check if back or forward button is pressed.
+
+    this.currentPath.subscribe(path => {
+      history.pushState({path: path}, path, window.location.href)
+    })
+
+    this.location.onPopState(() => {
+      storage.goBackDirectory()
+    });
   }
 
 }
